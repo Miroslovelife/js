85bdda0871d4db86b4c5a24a378b5f689ea0f9bd
Miroslav <m1rsi4all@gmail.com>
2023-10-09 00:23:19 +0300

    JS learn

---
+++ app.js
@@ -0,0 +1,23 @@
+// var arr = new Array
+// arr.push("fawf", 2)
+
+// console.log(arr)
+
+// arr.unshift("pidor")
+
+// console.log(arr)
+
+
+
+// console.log(arr.pop())
+
+
+// console.log(arr)
+
+// console.log(arr.shift())
+
+// console.log(arr)
+
+var arr = [1, 3, 5, 2];
+
+console.log(arr.sort(function(a, b) {return b - a;}));
